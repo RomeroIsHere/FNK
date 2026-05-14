0d98deb4745c44c2adef6465269a7cc65c4124cc
Hayumin <hayumigumi@gmail.com>
2026-05-13 20:03:37 -0600

    Add Found and Sought UITables

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,6 +1,146 @@
 <a href="/">
-    <svg class="size-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-        <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
-    </svg>
+    
+<svg
+    class="size-20"
+   width="1000mm"
+   height="1000mm"
+   viewBox="0 0 1000 1000"
+   version="1.1"
+   id="svg1"
+   sodipodi:docname="HijackPilot.svg"
+   inkscape:version="1.4.2 (f4327f4, 2025-05-13)"
+   inkscape:export-filename="HijackPilot.png"
+   inkscape:export-xdpi="96"
+   inkscape:export-ydpi="96"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   {{ $attributes }}>
+  <sodipodi:namedview
+     id="namedview1"
+     pagecolor="#505050"
+     bordercolor="#ffffff"
+     borderopacity="1"
+     inkscape:showpageshadow="0"
+     inkscape:pageopacity="0"
+     inkscape:pagecheckerboard="1"
+     inkscape:deskcolor="#505050"
+     inkscape:document-units="mm"
+     inkscape:zoom="0.18151129"
+     inkscape:cx="1010.9564"
+     inkscape:cy="1495.7747"
+     inkscape:window-width="1920"
+     inkscape:window-height="1017"
+     inkscape:window-x="-8"
+     inkscape:window-y="-8"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="g41"
+     inkscape:export-bgcolor="#ffffff00" />
+  <defs
+     id="defs1">
+    <linearGradient
+       id="linearGradient23"
+       inkscape:collect="always">
+      <stop
+         style="stop-color:#213773;stop-opacity:1;"
+         offset="0.46204621"
+         id="stop22" />
+      <stop
+         style="stop-color:#327f48;stop-opacity:0;"
+         offset="1"
+         id="stop23" />
+    </linearGradient>
+    <linearGradient
+       inkscape:collect="always"
+       xlink:href="#linearGradient23"
+       id="linearGradient22"
+       x1="110.78282"
+       y1="521.60699"
+       x2="886.57831"
+       y2="521.60699"
+       gradientUnits="userSpaceOnUse" />
+    <clipPath
+       clipPathUnits="userSpaceOnUse"
+       id="clipPath41">
+      <use
+         x="0"
+         y="0"
+         xlink:href="#g40"
+         id="use41" />
+    </clipPath>
+  </defs>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1">
+    <path
+       sodipodi:type="star"
+       style="fill:#43c093;stroke:none;stroke-width:25;stroke-linecap:round;fill-opacity:1"
+       id="path3"
+       inkscape:flatsided="true"
+       sodipodi:sides="7"
+       sodipodi:cx="523.30304"
+       sodipodi:cy="457.70798"
+       sodipodi:r1="521.67438"
+       sodipodi:r2="260.83719"
+       sodipodi:arg1="-2.7029398"
+       sodipodi:arg2="-2.2541409"
+       inkscape:rounded="0"
+       inkscape:randomized="0"
+       d="M 51.018369,236.14231 402.06538,-49.682982 844.40682,46.567469 1044.9506,452.41511 852.68308,862.24839 412.38571,967.45448 55.611348,688.81107 Z"
+       inkscape:transform-center-x="-24.832074"
+       inkscape:transform-center-y="1.1579354"
+       transform="matrix(1.0061048,0,0,0.98315129,-51.329828,48.845888)" />
+    <g
+       id="g2-8"
+       transform="translate(56.626506,-7.150499)">
+      <path
+         style="fill:url(#linearGradient22);stroke:none;stroke-width:25;stroke-linecap:round;fill-opacity:1"
+         d="m 110.78282,467.91165 c 0,0 79.27792,116.36199 92.96745,135.41668 13.68954,19.0547 87.73682,10.01096 130.97043,1.75068 68.27824,-13.04535 146.87084,-63.17995 146.87084,-63.17995 l -15.32698,255.2082 c 0,0 44.76777,-14.40602 62.19215,-23.14587 17.42438,-8.73985 100.68272,-292.32693 100.68272,-292.32693 0,0 120.21547,-82.32779 180.94034,-121.48669 60.72487,-39.1589 87.45386,-61.46907 72.37065,-87.46014 -16.21396,-27.93956 -72.46325,-30.29775 -104.62879,-23.14584 -32.16554,7.15191 -511.06827,260.64226 -511.06827,260.64226 0,0 -90.34271,-32.6393 -111.99038,-48.17633 -22.7788,1.64713 -43.98016,5.90393 -43.98016,5.90393 z"
+         id="path1-2"
+         sodipodi:nodetypes="czscczczszccc" />
+      <path
+         style="fill:#35327f;stroke:none;stroke-width:25;stroke-linecap:round;fill-opacity:1"
+         d="m 469.36932,383.36688 -97.66379,-43.73007 c 0,0 7.69347,-6.43736 24.78034,-18.9497 19.84146,-8.20186 169.08957,16.03436 169.08957,16.03436 z"
+         id="path2-4"
+         sodipodi:nodetypes="ccccc" />
+    </g>
+    <g
+       id="g2"
+       transform="translate(93.290796,-49.560736)">
+      <path
+         style="fill:#000000;stroke:none;stroke-width:25;stroke-linecap:round"
+         d="m 110.78282,467.91165 c 0,0 79.27792,116.36199 92.96745,135.41668 13.68954,19.0547 87.73682,10.01096 130.97043,1.75068 68.27824,-13.04535 146.87084,-63.17995 146.87084,-63.17995 l -15.32698,255.2082 c 0,0 44.76777,-14.40602 62.19215,-23.14587 17.42438,-8.73985 100.68272,-292.32693 100.68272,-292.32693 0,0 120.21547,-82.32779 180.94034,-121.48669 60.72487,-39.1589 87.45386,-61.46907 72.37065,-87.46014 -16.21396,-27.93956 -72.46325,-30.29775 -104.62879,-23.14584 -32.16554,7.15191 -511.06827,260.64226 -511.06827,260.64226 0,0 -90.34271,-32.6393 -111.99038,-48.17633 -22.7788,1.64713 -43.98016,5.90393 -43.98016,5.90393 z"
+         id="path1"
+         sodipodi:nodetypes="czscczczszccc" />
+      <path
+         style="fill:#000000;stroke:none;stroke-width:25;stroke-linecap:round"
+         d="m 469.36932,383.36688 -97.66379,-43.73007 c 0,0 7.69347,-6.43736 24.78034,-18.9497 19.84146,-8.20186 169.08957,16.03436 169.08957,16.03436 z"
+         id="path2"
+         sodipodi:nodetypes="ccccc" />
+    </g>
+    <g
+       id="g41"
+       clip-path="url(#clipPath41)">
+      <g
+         inkscape:label="Clip"
+         id="g40">
+        <path
+           style="fill:#000000;stroke:none;stroke-width:25;stroke-linecap:round"
+           d="m 110.78282,467.91165 c 0,0 79.27792,116.36199 92.96745,135.41668 13.68954,19.0547 87.73682,10.01096 130.97043,1.75068 68.27824,-13.04535 146.87084,-63.17995 146.87084,-63.17995 l -15.32698,255.2082 c 0,0 44.76777,-14.40602 62.19215,-23.14587 17.42438,-8.73985 100.68272,-292.32693 100.68272,-292.32693 0,0 120.21547,-82.32779 180.94034,-121.48669 60.72487,-39.1589 87.45386,-61.46907 72.37065,-87.46014 -16.21396,-27.93956 -72.46325,-30.29775 -104.62879,-23.14584 -32.16554,7.15191 -511.06827,260.64226 -511.06827,260.64226 0,0 -90.34271,-32.6393 -111.99038,-48.17633 -22.7788,1.64713 -43.98016,5.90393 -43.98016,5.90393 z"
+           id="path38"
+           sodipodi:nodetypes="czscczczszccc"
+           transform="translate(93.290795,-49.560737)" />
+      </g>
+      <path
+         style="fill:#3aa5e4;fill-opacity:1;stroke:#29594e;stroke-width:3;stroke-linecap:round;stroke-dasharray:none;stroke-opacity:1"
+         d="m 888.64239,225.02539 38.94398,2.6867 54.15349,-23.63847 -7.42726,-12.76534 z"
+         id="path37"
+         sodipodi:nodetypes="ccccc" />
+    </g>
+  </g>
+</svg>
+
 </a>
--- resources/views/livewire/found.blade.php
+++ resources/views/livewire/found.blade.php
@@ -1,11 +1,13 @@
+
 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+    
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
-        Found Items!
+        Found Items: {{ $count=10 }}
     </h1>
     <div class="flex flex-col">
-        @for ($i = 0; $i < 10; $i++)
+        @for ($i = 0; $i < $count; $i++)
             <div class="bg-blue-200 m-2 p-3 lg:p-4 rounded-lg">
                 {{ $i }}
             </div>
