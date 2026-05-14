88b1341d83de0e242c5033cacc24d784c813ee79
RomeroIsHere <145238541+RomeroIsHere@users.noreply.github.com>
2026-05-14 01:27:01 -0600

    Pagination for Items (#4)
    * Add Pagination to Sought
    
    * Added Pagination for found Table
    
    * Semi-fix: Paginate on Same page at different times
    
    * Actual fix and Detection for multiPagination
    
    ---------
    
    Co-authored-by: Hayumin <hayumigumi@gmail.com>

--- app/Livewire/Found.php
+++ app/Livewire/Found.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Url;
+use App\Models\FoundItem;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Found extends Component
 {
+    #[Url]
+    public $sought= '';
+    use WithPagination;
+    public $datos =[];
+    public $count = 0;
+    public function mount(){
+        $this->count=FoundItem::count();
+    }
+    public function found(){
+        return FoundItem::paginate(5, ['*'], "found");
+    }
     public function render()
     {
         return view('livewire.found');
