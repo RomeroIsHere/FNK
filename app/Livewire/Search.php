afa2522f75331e6eb3a35c16e2882ce84a9699bd
Hayumin <hayumigumi@gmail.com>
2026-05-14 00:43:22 -0600

    Add Pagination to Sought

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -2,11 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\SoughtItem;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Search extends Component
 {
+    use WithPagination;
     public $datos =[];
+    public $count = 0;
+    public function mount(){
+        $this->count=SoughtItem::count();
+    }
+    public function sought(){
+        return SoughtItem::paginate(5);
+    }
+
     public function render()
     {
         return view('livewire.search');
