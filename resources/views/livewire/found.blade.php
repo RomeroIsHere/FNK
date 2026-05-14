2113089ca8c961de35ba623c2b7e9a0d5df89dd0
Hayumin <hayumigumi@gmail.com>
2026-05-13 20:07:19 -0600

    Clarify that It's Unclaimed items

--- resources/views/livewire/found.blade.php
+++ resources/views/livewire/found.blade.php
@@ -4,7 +4,7 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
-        Found Items: {{ $count=10 }}
+        Found Unclaimed Items: {{ $count=10 }}
     </h1>
     <div class="flex flex-col">
         @for ($i = 0; $i < $count; $i++)
