fe71fc24483a37d1a3d723f072a817eb2162bd0e
Hayumin <hayumigumi@gmail.com>
2026-05-17 15:32:34 -0600

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\FoundItem;
+use App\Models\SoughtItem;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +17,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
+        User::factory()->create([
+            'name' => 'Guillermo Romero Cuevas',
+            'email' => 'guillermoromecu@gmail.com',
+        ]);
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        FoundItem::factory(20)->create();
+        SoughtItem::factory(20)->create();
+        $founds=FoundItem::all();
+        SoughtItem::all()->each(function ($sought) use ($founds) {
+            $sought->founditems()->attach(
+                $founds->random(rand(min(0,$founds->count()),$founds->count()))->pluck('id')->toArray()
+            );
+        });
     }
 }
